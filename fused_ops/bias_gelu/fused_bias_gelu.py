ed892b364697914cc39fc6186963483b83874e68
pdlan <pengdinglan@gmail.com>
2021-05-25 05:14:47 +0000

    Change gelu_tanh to gelu_fast

--- fused_ops/bias_gelu/fused_bias_gelu.py
+++ fused_ops/bias_gelu/fused_bias_gelu.py
@@ -25,16 +25,16 @@ class BiasTorchGeLUFunction(torch.autograd.Function):
             grad_input_bias = grad_input
         return grad_input, grad_input_bias
 
-class BiasTanhGeLUFunction(torch.autograd.Function):
+class BiasFastGeLUFunction(torch.autograd.Function):
     @staticmethod
     def forward(ctx, input, bias):
         ctx.save_for_backward(input, bias)
-        return fused_bias_gelu_cuda.forward_tanh(input, bias)
+        return fused_bias_gelu_cuda.forward_fast(input, bias)
 
     @staticmethod
     def backward(ctx, grad_output):
         input, bias = ctx.saved_tensors
-        grad_input = fused_bias_gelu_cuda.backward_tanh(input, bias, grad_output)
+        grad_input = fused_bias_gelu_cuda.backward_fast(input, bias, grad_output)
         if len(grad_input.size()) > 1:
             sizes = grad_input.size()
             grad_input_bias = grad_input.view(-1, sizes[-1]).sum(dim=0)
