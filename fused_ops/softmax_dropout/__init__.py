9b692d209f644460b1d3852f1e633fbae008cfc8
Guolin Ke <guoke@microsoft.com>
2021-07-13 06:25:01 +0000

    fix init

--- fused_ops/softmax_dropout/__init__.py
+++ fused_ops/softmax_dropout/__init__.py
@@ -1,9 +1,9 @@
 try:
     import torch
     import fused_softmax_dropout_fast_cuda
-    from .fused_softmax_dropout_fast import SoftmaxDropoutFast, softmax_dropout
+    from .fused_softmax_dropout import softmax_dropout
     del torch
     del fused_softmax_dropout_fast_cuda
-    del fused_softmax_dropout_fast
+    del fused_softmax_dropout
 except ImportError as err:
     print("cannot import kernels, please install the package")
