fa678c1a1a6ef5f9a75ced48fff088431d0977d1
Guolin Ke <guolin.ke@outlook.com>
2022-01-11 10:14:36 +0800

    fix import

--- fused_ops/__init__.py
+++ fused_ops/__init__.py
@@ -3,15 +3,9 @@ import torch
 import warnings
 
 from . import *
-from .bias_dropout_add_module import *
-from .bias_gelu_module import *
 from .layernorm_module import *
 from .softmax_dropout_module import *
-from .xentropy_module import *
-del bias_dropout_add_module
-del bias_gelu_module
 del layernorm_module
 del softmax_dropout_module
-del xentropy_module
 del torch
 del warnings
